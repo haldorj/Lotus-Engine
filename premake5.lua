a89e708be0799e925b9b3529b651c814677fd7b1
haldorj <haldorjh@gmail.com>
2023-12-22 09:12:53 +0100

    added premake5 and submodules
    - spdlog
    - glfw
    - imgui
    - spdlog

---
+++ premake5.lua
@@ -0,0 +1,141 @@
+workspace "Lotus"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Lotus/vendor/GLFW/include"
+IncludeDir["ImGui"] = "Lotus/vendor/imgui"
+IncludeDir["glm"] = "Lotus/vendor/glm"
+
+include "Lotus/vendor/GLFW"
+include "Lotus/vendor/imgui"
+
+project "Lotus"
+	location "Lotus"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "lotuspch.h"
+	pchsource "Lotus/src/lotuspch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
+	}
+
+	links 
+	{ 
+		"GLFW",
+		"ImGui",
+		"opengl32.lib",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"LOTUS_PLATFORM_WINDOWS",
+			"LOTUS_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
+
+
+	filter "configurations:Debug"
+		defines "LOTUS_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "LOTUS_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "LOTUS_DIST"
+		runtime "Release"
+		optimize "on"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Lotus/vendor/spdlog/include",
+		"Lotus/src",
+		"Lotus/vendor",
+		"%{IncludeDir.glm}"
+	}
+
+	links
+	{
+		"Lotus"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"LOTUS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LOTUS_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "LOTUS_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "LOTUS_DIST"
+		runtime "Release"
+		optimize "on"
